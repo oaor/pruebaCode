989b90abadee8e71daf3ea49efe3607a5ed488bd
Olga Angelica <angortizr@gmail.com>
2022-09-07 11:00:56 -0500

    Hola Mundo!

---
+++ src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
@@ -0,0 +1,22 @@
+package com.UdeA.Ciclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication (exclude = SecurityAutoConfiguration.class)
+public class Ciclo3Application {
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hola Ciclo 3... Saldremos vivos de ésto!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ciclo3Application.class, args);
+	}
+
+}
