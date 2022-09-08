42faee65089ccc83d5a0fcfc4f974a6bc8d90335
Olga Angelica <angortizr@gmail.com>
2022-09-08 10:19:02 -0500

    Crear entidad MovimientoDinero

--- src/main/java/com/UdeA/Ciclo3/modelos/MovimientoDinero.java
+++ src/main/java/com/UdeA/Ciclo3/modelos/MovimientoDinero.java
@@ -1,4 +1,67 @@
 package com.UdeA.Ciclo3.modelos;
 
+import javax.persistence.*;
+
+@Entity
+@Table (name = "Movimientos")
 public class MovimientoDinero {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private int id;
+    private long monto;
+    private String concepto;
+    @ManyToOne
+    @JoinColumn(name = "empleado_id")
+    private Empleado empleado;
+    private String rol;
+
+    public MovimientoDinero() {
+    }
+
+    public MovimientoDinero(long monto, String concepto, Empleado empleado, String rol) {
+        this.monto = monto;
+        this.concepto = concepto;
+        this.empleado = empleado;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getMonto() {
+        return monto;
+    }
+
+    public void setMonto(long monto) {
+        this.monto = monto;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
 }
