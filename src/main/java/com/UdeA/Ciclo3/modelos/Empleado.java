ff1e9a31b6d0c97e4c4defd91066f6f64859c485
Olga Angelica <angortizr@gmail.com>
2022-09-08 08:38:10 -0500

    Crear entidad Empleado

--- src/main/java/com/UdeA/Ciclo3/modelos/Empleado.java
+++ src/main/java/com/UdeA/Ciclo3/modelos/Empleado.java
@@ -10,7 +10,61 @@ public class Empleado {
     private int id;
     private String nombreEmpleado;
     private String correoEmpleado;
+    @ManyToOne
+    @JoinColumn(name = "empresa_id")
+    private Empresa empresa;
     private String rol;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombreEmpleado, String correoEmpleado, Empresa empresa, String rol) {
+        this.nombreEmpleado = nombreEmpleado;
+        this.correoEmpleado = correoEmpleado;
+        this.empresa = empresa;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreEmpleado() {
+        return nombreEmpleado;
+    }
+
+    public void setNombreEmpleado(String nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public String getCorreoEmpleado() {
+        return correoEmpleado;
+    }
+
+    public void setCorreoEmpleado(String correoEmpleado) {
+        this.correoEmpleado = correoEmpleado;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+
 
 
 }
